c40918022e73848704ed9013c7f0c333957d2339
VictorArd <victor.airiaud@outlook.fr>
2019-03-14 17:26:48 +0100

    Final Website without finish image for event

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -1,2 +1,42 @@
-class EventsController < ApplicationController
+class Admin::EventsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :check_if_admin
+
+  def index
+    @events = Event.all.where(validated: nil)
+  end
+
+  def edit
+    @event = Event.find(params[:id])
+  end
+
+  def update
+    @event = Event.find(params[:id])
+
+    if params[:validated].present?
+      @event.update(validated: params[:validated])
+      flash[:info] = "L'évènement à été validé"
+      redirect_to admin_events_path
+    else
+      flash[:warning] = "Vous n'avez pas coché la case pour valider l'évènement"
+      render :edit
+    end
+  end
+
+  def destroy
+    @event = Event.find(params[:id])
+    @event.destroy
+    flash[:warning] = "L'évènement à été supprimé"
+    redirect_to admin_events_path
+  end
+
+
+  private
+
+  def check_if_admin
+    unless current_user.is_admin
+      flash[:danger] = "Vous n'êtes pas Administrateur"
+      redirect_back(fallback_location: root_path)
+    end
+  end
 end
--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,2 +1,47 @@
-class UsersController < ApplicationController
+class Admin::UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :check_if_admin
+
+	def index
+    @users = User.all.sort
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      description: params[:description]
+      )
+      if params[:is_admin].present?
+        @user.update(is_admin: params[:is_admin])
+      end
+
+      flash[:notice] = "Les informations de l'utilisateur ont été modifiées"
+      redirect_to admin_users_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    flash[:notice] = "L'utilisateur à bien été supprimé"
+    redirect_to admin_users_path
+  end
+
+
+  private
+
+  	def check_if_admin
+  		unless current_user.is_admin
+	      flash[:danger] = "Vous n'êtes pas Administrateur"
+	      redirect_back(fallback_location: root_path)
+    	end
+    end
 end
